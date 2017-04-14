9cd3382280fb0423c775ca0d988419b4910555eb
o.riffaud <o.riffaud@astellia.com>
2017-04-14 18:04:39 +0200

    Adding Lists and CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,44 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   selected = 'drive';
+  driver = "Vous";
+  passengers = ["François","Xavier"];
+  donotlist = ["Marie"];
+
+constructor() { }
 
   drive(): void {
     this.selected = 'drive';
+    this.driver = "Vous";
+    if (this.passengers.indexOf("Vous") != -1) {
+      this.passengers.splice(this.passengers.indexOf("Vous"), 1);
+    }
+    if (this.donotlist.indexOf("Vous") != -1) {
+      this.donotlist.splice(this.donotlist.indexOf("Vous"), 1);
+    }
+
   }
+
   passenger(): void {
     this.selected = 'passenger';
+    this.driver = "";
+    if (this.passengers.indexOf("Vous") == -1) {
+      this.passengers.push("Vous");
+    }
+    if (this.donotlist.indexOf("Vous") != -1) {
+      this.donotlist.splice(this.donotlist.indexOf("Vous"), 1);
+    }
+
   }
+
   donot(): void {
     this.selected = 'donot';
+    this.driver = "";
+    if (this.passengers.indexOf("Vous") != -1) {
+      this.passengers.splice(this.passengers.indexOf("Vous"), 1);
+    }
+    if (this.donotlist.indexOf("Vous") == -1) {
+      this.donotlist.push("Vous");
+    }
   }
 }
